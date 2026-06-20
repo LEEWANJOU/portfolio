c53a915948b0c878469968ac4d723b78bf3bbefd
LEEWANJOU <dogo932@gmail.com>
2026-06-21 02:33:05 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const distance = Math.abs(viewportCenter - elementCenter);
       const maxDistance = window.innerHeight;
       
-      const scale = Math.max(0.3, 1.5 - (distance / maxDistance) * 1.2);
+      const scale = Math.max(0.3, 1.8 - (distance / maxDistance) * 1.5);
       const opacity = Math.max(0.5, 1 - (distance / maxDistance) * 0.5);
       
       const img = wrapper.querySelector('.portfolio-page');
