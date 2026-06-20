3f4f4234fbc7f7ba0b9c16c67531863df6649c0a
LEEWANJOU <dogo932@gmail.com>
2026-06-21 04:10:21 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var main = document.getElementById('main');
   var heroImage = document.getElementById('heroImage');
 
-  var availablePages = [2,3,4,5,7,8,9,10,11,13,14,15,16,17,19,20,21,22,23,24,25,26,27,28,29,30,31];
+  var availablePages = [3,4,5,7,9,11,14,16,17,20,21,22,23,24,25,26,27,28,29,30,31];
   var imageInterval = null;
 
   function showRandomImage() {
